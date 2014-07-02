39eeaeebc7be008ddf1e4201eb74721617858fbd
jparker85 <jparker85@gmail.com>
2014-07-02 13:45:47 -0400

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+// CONSTRUCTORS
+// Here is where you'll create your constructors for you player and monster.
+// e.g. function Monster(){};
+// e.g. function Player(){};
+
+
+// GLOBAL VARIABLES
+// You'll also want to create variables for the specific players and monsters.
+// e.g. var purplePeopleEater = new Monster();
+// e.g. var tinaFey = new Player();
+// e.g. var attackMode = function(target){ Some code that produces an attack - pew, pew! };
+
+
+
+// ACTIONS
+// This is where jQuery will come into play and where you'll set a lot of your
+// interactions for players and monsters. (e.g. player selection, attack interactions)
+// e.g. $('.purple-people-eater').click(function () { Some code that attacks the monster! };
