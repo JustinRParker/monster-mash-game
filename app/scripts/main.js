459f0cda8fa1412dd0984aaec8921fa8bc901462
jparker85 <jparker85@gmail.com>
2014-07-06 23:26:37 -0400

    MMG progress

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,64 +1,84 @@
-// CONSTRUCTORS
-// Here is where you'll create your constructors for you player and monster.
-// e.g. function Monster(){};
-// e.g. function Player(){};
-function Player(options){
-  options = options || {};
-  this.name = options.name;
+// Allows page to load before running scripts
+//http://api.jquery.com/ready/
+
+$(function(){
+
+  $(".ninjaTurtleImage").hide();
+  $(".ninjaImage").hide();
+  $(".godzillaMonster").hide();
+  // CONSTRUCTORS
+  // Here is where you'll create your constructors for you player and monster.
+
+  function Player(options){
+    options = options || {};
+    this.name = options.name;
+  }
+
+  function Monster(options){
+    options = options || {};
+    this.name = options.name;
+    this.health = 100;
+  }
+  // GLOBAL VARIABLES
+  // You'll also want to create variables for the specific players and monsters.
+
+  var ninjaTurtle = new Player({
+    name: 'Ninja Turtle'
+  });
+
+  var priest = new Player({
+    name: 'priest'
+  });
+
+  var godzilla = new Monster({
+    name: 'godzilla'
+  });
+
+  // ACTIONS
+  // This is where jQuery will come into play and where you'll set a lot of your
+  // interactions for players and monsters. (e.g. player selection, attack interactions)
+  // e.g. $('.purple-people-eater').click(function () { Some code that attacks the monster! };
+
+  $('.ninjaTurtlePlayer').click(function(){
+    event.preventDefault();
+
+    $(".Player").hide();
+    alert('Ninja Turtle FIGHT! \nCLick on Godzilla to kill him!');
+    $(".ninjaTurtleImage").show();
+    $(".godzillaMonster").show();
+  });
+
+  $('.ninjaPlayer').click(function(){
+    event.preventDefault();
+    $(".Player").hide();
+    alert('Ninja FIGHT! \nCLick on Godzilla to kill him!');
+    $(".ninjaImage").show();
+    $(".godzillaMonster").show();
+  });
+
+  // $(".btnAttack").button();
+
+  $('.attackGodzilla').click(function(){
+    event.preventDefault();
+    //Change health of monster
+    godzilla.health = godzilla.health - 10;
+
+    //Check to see if monster's health is 0
+    if (godzilla.health === 0){
+
+      //If true alert player that they won.
+      alert('You won!');
+      // todo: logic for play again
+      if (window.confirm("Play Again?")) {
+  window.open("http://localhost:9000");
 }
-
-function Monster(options){
-  options = options || {};
-  this.name = options.name;
-  this.health = 0;
-}
-// GLOBAL VARIABLES
-// You'll also want to create variables for the specific players and monsters.
-// e.g. var purplePeopleEater = new Monster();
-// e.g. var tinaFey = new Player();
-// e.g. var attackMode = function(target){ Some code that produces an attack - pew, pew! };
-
-var nun = new Player({
-  name: 'nun'
-});
-
-var priest = new Player({
-  name: 'priest'
-});
-
-var godzilla = new Monster({
-  name: 'godzilla'
-});
-
-// ACTIONS
-// This is where jQuery will come into play and where you'll set a lot of your
-// interactions for players and monsters. (e.g. player selection, attack interactions)
-// e.g. $('.purple-people-eater').click(function () { Some code that attacks the monster! };
-
-$('.nunPlayer').click(function(){
-  event.preventDefault();
-  $(".Player").hide();
-  alert('Nun FIGHT!');
-  $(".nunImage").show();
-});
-
-$('.priestPlayer').click(function(){
-  event.preventDefault();
-  alert('Priest FIGHT!');
-  (".priestImage").show();
-});
-
-$(".btnAttack").button();
-var health = health + 5;
-var attackMode = function ( event ){
-    if (Monster.health === 20) {
-      return Monster.health = health + 5;
-      alert(Monster.health)
     }
-  else
+    else
     {
-      alert('You Won!');
-      window.prompt('Play again?')
+      //else alert health of monster
+      alert("Godzillia's Health:" + godzilla.health);
     }
 
-};
+    // $(".priestImage").show();
+  });
+});
